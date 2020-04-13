3b9514b91dd88b2cc28f2821dfc7337210cd16d5
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-04-13 18:00:46 -0300

    feat: create appointment

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.post('/', (req, res) => {
+  const { provider, date } = req.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate
+  );
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+
+  return res.json(appointment);
+});
+
+export default appointmentsRouter;
