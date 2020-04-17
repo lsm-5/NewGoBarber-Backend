f5992b45776297c89c166979aa971b70b2c7254a
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-04-17 17:10:03 -0300

    feat: update and create avatar user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,14 @@
-import { Router } from 'express';
+import { Router, request } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -17,5 +24,24 @@ usersRouter.post('/', async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+      const user = await updateUserAvatar.execute({
+        user_id: req.user.id,
+        avatarFilename: req.file.filename,
+      });
+      delete user.password;
+      res.json(user);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  }
+);
 
 export default usersRouter;
