d9846e2fb105bdc80d9a05efe25b7def8d29dd94
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-04-17 18:29:53 -0300

    refactor: tratament error

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,21 +5,17 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const authenticateUserService = new AuthenticateUserService();
-    const userWithToken = await authenticateUserService.execute({
-      email,
-      password,
-    });
+  const authenticateUserService = new AuthenticateUserService();
+  const userWithToken = await authenticateUserService.execute({
+    email,
+    password,
+  });
 
-    delete userWithToken.user.password;
+  delete userWithToken.user.password;
 
-    return res.json(userWithToken);
-  } catch (err) {
-    res.status(err.statusCode).json({ error: err.message });
-  }
+  return res.json(userWithToken);
 });
 
 export default sessionsRouter;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,18 +11,14 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const createUser = new CreateUserService();
+  const { name, email, password } = req.body;
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({ name, email, password });
+  const user = await createUser.execute({ name, email, password });
 
-    delete user.password;
+  delete user.password;
 
-    return res.json(user);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+  return res.json(user);
 });
 
 usersRouter.patch(
@@ -30,17 +26,13 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (req, res) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-      const user = await updateUserAvatar.execute({
-        user_id: req.user.id,
-        avatarFilename: req.file.filename,
-      });
-      delete user.password;
-      res.json(user);
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
+    const updateUserAvatar = new UpdateUserAvatarService();
+    const user = await updateUserAvatar.execute({
+      user_id: req.user.id,
+      avatarFilename: req.file.filename,
+    });
+    delete user.password;
+    res.json(user);
   }
 );
 
